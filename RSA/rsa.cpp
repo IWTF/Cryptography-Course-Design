5f34918db059899a4a5097e6d6b0aa2dc599c955
IWTF <hzw2658177@163.com>
2020-01-07 19:25:24 +0800

    RSA辅助函数完成，开始加密/解密算法

--- RSA/rsa.cpp
+++ RSA/rsa.cpp
@@ -10,31 +10,32 @@ using namespace std;
 
 typedef bitset<32> DWord;
 
-unsigned long N;
+unsigned long miniPrime[100];
+int num = 0;
 
 #define randA2B(a, b) (rand() % (b-a+1))+ a;
 
 // 自定义取模函数，防止大数溢出 (a^q)mod(m)
-unsigned long modN(long a, long q) {
+unsigned long modN(long a, long q, int n) {
   unsigned long addition = 1;
   unsigned long temp;
-  a = fmod(a, N);
+  a = fmod(a, n);
   while (q > 1) {
-    temp = fmod(pow(a, 2), N);
+    temp = fmod(pow(a, 2), n);
     if (q%2 == 1)
-      addition = fmod((addition*a), N);
+      addition = fmod((addition*a), n);
     q = q/2;
     a = temp;
   }
-  return fmod((temp*addition), N);
+  return fmod((temp*addition), n);
 }
 
 // 测似乎以a为底时，n是否为合数
-int is_composite(unsigned long a, unsigned long d, unsigned long s) {
-  if (modN(a, d) == 1)
+int is_composite(unsigned long a, unsigned long d, unsigned long s, unsigned long n) {
+  if (modN(a, d, n) == 1)
     return FALSE;
   for (int i=0; i<s; i++) {
-    if (modN(a, pow(2, i)*d) == (N-1))
+    if (modN(a, pow(2, i)*d, n) == (n-1))
       return FALSE;
   }
   return TRUE;
@@ -68,7 +69,7 @@ int is_probable_prime(unsigned long n, int accuracy=5) {
   srand((unsigned)time(NULL));
   for (int i=0; i<accuracy; i++) {
     unsigned long randNum = randA2B(2, n-1);
-    if (is_composite(randNum ,d, s))
+    if (is_composite(randNum ,d, s, n))
       return FALSE;
   }
   return TRUE;
@@ -80,20 +81,52 @@ unsigned long Pseudoprime(int n=32) {
   
   srand((unsigned)time(NULL));
   while (TRUE) {
+    // 产生一个大奇数
   	for (int i=(n-1); i>0; i--) {
       temp[i] = randA2B(0, 1);
     }
     temp[0] = 1;
   
-    N = temp.to_ulong();
+    unsigned long N = temp.to_ulong();
+    int flag = 0;
+    for (int i=0; i<99; i++) {
+    	if (fmod(N, miniPrime[i]) == 0) {
+    		flag = 1;
+			  break;
+		  }
+	  }
+	  if (flag == 1)
+	  	continue;
     if (is_probable_prime(N))
       return N;
+  }
+} 
+
+// 初始化生成一些小的素数，提高大素数产生效率 
+void initMiniPrime() {
+	for (int i=0; i<530; i++) {
+		if (is_probable_prime(i, 10)) {
+			miniPrime[num++] = i;
+		}	
+	}
+}
+
+// 扩展欧几里得算法
+int Extended_Euclid(int a,int &x,int b,int &y,int c) {
+  if(b==0){x=c/a,y=0;return a;}
+  else
+  {
+    int p=Extended_Euclid(b,x,a%b,y,c);
+    int x_=x,y_=y;
+    x=y_; y=x_-a/b*y_; 
+    return p;
   }
 }
 
 int main() {
+	initMiniPrime();
 	
-	unsigned long a = Pseudoprime();
+	int a = Pseudoprime(18);
 	cout<<a;
 	return 0;
 }
